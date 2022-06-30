b207fca1f253fa77bae7663a6251bcde1f2af024
Yunwei Xiao <xiaoluoboding@gmail.com>
2022-06-30 11:01:55 +0800

    feat: add producthunt badge

--- frontend/components/TheFooter.vue
+++ frontend/components/TheFooter.vue
@@ -6,7 +6,7 @@
       <div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
         <div>
           <div class="flex justify-center text-violet-500 sm:justify-start">
-            <img src="/logo.svg" class="h-8" />
+            <img src="/logo-light.svg" class="h-6" />
           </div>
 
           <p
@@ -16,7 +16,22 @@
             <span class="text-neon">supercharge</span> your next project.
           </p>
 
-          <ul class="flex justify-center gap-6 mt-8 md:gap-8 sm:justify-start">
+          <div class="mt-6 flex justify-center sm:justify-start">
+            <a
+              href="https://www.producthunt.com/posts/tech-stack-tools?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-tech&#0045;stack&#0045;tools"
+              target="_blank"
+              ><img
+                src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=351122&theme=neutral"
+                alt="tech&#0045;stack&#0046;tools - Discover&#0032;our&#0032;curated&#0032;list&#0032;of&#0032;creative&#0032;tools | Product Hunt"
+                style="width: 250px; height: 54px"
+                width="250"
+                height="54"
+            /></a>
+          </div>
+
+          <ul
+            class="flex justify-center gap-x-6 mt-6 md:gap-x-8 sm:justify-start"
+          >
             <li>
               <a
                 href="https://twitter.com/TechStackTools"
@@ -46,18 +61,7 @@
                 class="text-violet-700 transition hover:text-violet-700/75"
               >
                 <span class="sr-only">GitHub</span>
-                <svg
-                  class="w-6 h-6"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
+                <carbon:logo-github class="h-6 w-6" />
               </a>
             </li>
           </ul>
